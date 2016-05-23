1fcd53050ecaaf485412f59af7dfccc7e08ad92d
frank dingler <frankdingler@hotmail.com>
2016-05-23 20:57:48 +0200

    Keylistener aangemaakt en werkend gemaakt

--- ProjectFA/src/view/Main.java
+++ ProjectFA/src/view/Main.java
@@ -23,6 +23,8 @@ public class Main {
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   
         Spelpaneel spelpaneel = new Spelpaneel();
+        spelpaneel.setFocusable(true);
+        spelpaneel.requestFocus();
         frame.add(spelpaneel);
         frame.setVisible(true);
     }
--- ProjectFA/src/view/Spelpaneel.java
+++ ProjectFA/src/view/Spelpaneel.java
@@ -5,20 +5,41 @@
  */
 package view;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 /**
  *
  * @author Frank
  */
-public class Spelpaneel extends JPanel
+public class Spelpaneel extends JPanel implements KeyListener
 {
         public Spelpaneel()
         {
            setSize(1000,600);
            JLabel label = new JLabel("hallo");
+           addKeyListener(this);
            add(label);
         }
+
+    @Override
+    public void keyTyped(KeyEvent ke) {
+        
+    }
+
+    @Override
+    public void keyPressed(KeyEvent ke) {
+        if(ke.getKeyCode() == KeyEvent.VK_RIGHT)
+        {
+            JOptionPane.showMessageDialog(this, "hallo");
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent ke) {
+    }
 }
    
