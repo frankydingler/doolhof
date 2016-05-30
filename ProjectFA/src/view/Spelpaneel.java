43ed5cb337b6f7d05c13182eaf2ba042f45a960d
frank dingler <frankdingler@hotmail.com>
2016-05-30 16:40:07 +0200

    merge

--- ProjectFA/src/view/Spelpaneel.java
+++ ProjectFA/src/view/Spelpaneel.java
@@ -29,6 +29,7 @@ public class Spelpaneel extends JPanel implements KeyListener
         private Spelobject[][] objectlijst;
         private Speler speler;
         private int counter;
+        private boolean gebruikBazooka;
         
         public Spelpaneel()
         {
